d067b6ef7fefce6b87a7990806ac58c5397ba9b7
Luan Moraes Alencar <luan.moraees21@gmail.com>
2025-02-13 09:58:25 -0300

    criando grid de botoes

--- src/buttons.py
+++ src/buttons.py
@@ -1,10 +1,17 @@
-from PySide6.QtWidgets import QPushButton
+from PySide6.QtWidgets import QPushButton, QGridLayout
 from variables import MEDIUM_FONT_SIZE
 
 
 class Button(QPushButton):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, *kwargs)
+        self.configStyle()
 
     def configStyle(self):
-        self.setStyleSheet(f'font-size: {MEDIUM_FONT_SIZE}px')
+        font = self.font()
+        font.setPixelSize(MEDIUM_FONT_SIZE)
+        self.setFont(font)
+
+
+class ButtonsGrid(QGridLayout):
+    ...
--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from PySide6.QtWidgets import QApplication
-from buttons import Button
+from buttons import ButtonsGrid, Button
 from main_window import MainWindow
 from PySide6.QtGui import QIcon
 from variables import WINDOW_ICON_DIR
@@ -13,17 +13,21 @@ if __name__ == '__main__':
     setupTheme(app)
     window = MainWindow()
     window.setWindowTitle('Calculadora')
+    icon = QIcon(str(WINDOW_ICON_DIR))
 
     info = Info('Label principal')
-    window.addToVLayout(info)
+    window.addWidgetToVLayout(info)
 
     display = Display()
-    window.addToVLayout(display)
-    icon = QIcon(str(WINDOW_ICON_DIR))
+    window.addWidgetToVLayout(display)
     window.setWindowIcon(icon)
 
-    button = Button('Texto do botao')
-    window.addToVLayout(button)
+    buttonsGrid = ButtonsGrid()
+    window.vLayout.addLayout(buttonsGrid)
+
+    buttonsGrid.addWidget(Button('0'), 0, 0)
+    buttonsGrid.addWidget(Button('1'), 0, 1)
+    buttonsGrid.addWidget(Button('2'), 0, 2)
 
     window.adjustFixedSize()
     window.show()
